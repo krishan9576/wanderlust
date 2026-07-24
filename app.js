837635d164dde797248c71c9a0fe652b7cbdb640
krishan kumar <kkrkashyp@gmail.com>
2026-07-24 10:23:12 +0530

    Remove MongoStore crypto

--- app.js
+++ app.js
@@ -69,9 +69,6 @@ app.use(express.static(path.join(__dirname,"/public")));
 const store = MongoStore.create({
     
     mongoUrl:dbUrl,
-    crypto:{
-        secret: process.env.SECRET,
-    },
     touchAfter:24*3600,//for lazy update
 
  });
