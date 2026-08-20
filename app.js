68b6c8c48f204d4921b59884930aecc62c027d52
krishan kumar <kkrkashyp@gmail.com>
2026-08-20 20:11:32 +0530

    Fix Mongo session JSON parsing error

--- app.js
+++ app.js
@@ -68,11 +68,9 @@ app.use(express.static(path.join(__dirname,"/public")));
 
 const store = MongoStore.create({
     mongoUrl: dbUrl,
-    crypto: {
-        secret: process.env.SECRET,
-    },
     touchAfter: 24 * 3600
 });
+
 
  store.on("error",(err)=>{
     console.log("ERROR in MONGO SESSION STORE",err);
