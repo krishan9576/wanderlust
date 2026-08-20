abd3813ccad3f038e827bb35d255a621658a1db2
krishan kumar <kkrkashyp@gmail.com>
2026-08-20 19:24:22 +0530

    Restore Mongo session configuration

--- app.js
+++ app.js
@@ -68,7 +68,10 @@ app.use(express.static(path.join(__dirname,"/public")));
 
 const store = MongoStore.create({
     mongoUrl: dbUrl,
-    touchAfter: 24 * 3600
+    crypto: {
+        secret: process.env.SECRET,
+    },
+    touchAfter: 24 * 3600,
 });
 
  store.on("error",(err)=>{
